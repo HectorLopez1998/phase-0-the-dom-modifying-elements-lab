e548c9c7d63363fa14cb43e8e1300506ab621c5a
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-12-20 11:27:18 -0500

    commit tree bb6e559e27b3c276e24b1adce51e740bf5c2831c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// const newHeader = document.createElement('h1')
+// document.body.append(newHeader)
+// newHeader.textContent = 'j'
+// newHeader.id.add('victory')
+
+const newHeader = document.querySelector('#victory')
+newHeader.textContent = 'YOUR-NAME is the champion'
